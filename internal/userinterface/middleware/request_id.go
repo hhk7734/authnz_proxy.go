738c2ed38118d7b0fbe914c8ffd43febb94b3363
Hyeonki Hong <hhk7734@gmail.com>
2024-07-21 17:28:24 +0900

    feat(middleware): add request_id

---
+++ internal/userinterface/middleware/request_id.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/hhk7734/zapx.go"
+	"go.uber.org/zap"
+)
+
+func GinRequestIDMiddleware(generateIfNotExist bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		requestID := c.GetHeader("X-Request-Id")
+		if _, err := uuid.Parse(requestID); err != nil {
+			if generateIfNotExist {
+				requestID = uuid.New().String()
+				c.Request.Header.Set("X-Request-Id", requestID)
+			} else {
+				return
+			}
+		}
+		// Response
+		c.Header("X-Request-Id", requestID)
+
+		ctx := c.Request.Context()
+		ctx = zapx.WithFields(ctx, zap.String("request_id", requestID))
+		c.Request = c.Request.WithContext(ctx)
+
+		c.Next()
+	}
+}
