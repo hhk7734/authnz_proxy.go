91852165cb3d6e8d7979dbab0299c8fa98473c1f
Hyeonki Hong <hhk7734@gmail.com>
2024-07-22 02:27:31 +0900

    feat(config): add config option

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
@@ -16,6 +18,7 @@ const (
 
 func Load(pFlagSets ...*pflag.FlagSet) {
 	// pflag
+	pflag.CommandLine.String(CONFIG_KEY, "config.yaml", "config file path")
 	for _, f := range pFlagSets {
 		pflag.CommandLine.AddFlagSet(f)
 	}
@@ -24,24 +27,43 @@ func Load(pFlagSets ...*pflag.FlagSet) {
 	viper.BindPFlags(pflag.CommandLine)
 
 	// env
+	for _, env := range []string{".env", "../.env", "../../.env"} {
+		// If file exists, load it
+		if _, err := os.Stat(env); err == nil {
+			if err := godotenv.Load(env); err != nil {
+				panic(fmt.Errorf("failed to load env file: %w", err))
+			}
+			break
+		}
+	}
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
 	viper.AutomaticEnv()
 
 	// config file
-	viper.SetConfigName(".env")
-	viper.SetConfigType("dotenv")
+	configPath := viper.GetString(CONFIG_KEY)
+	dir := filepath.Dir(configPath)
+	base := filepath.Base(configPath)
+	ext := filepath.Ext(base)
+	viper.AddConfigPath(dir)
+	viper.SetConfigName(base)
+	if len(ext) > 1 {
+		viper.SetConfigType(ext[1:])
+	}
+
 	workDir, _ := os.Getwd()
 	for {
-		if _, err := os.Stat(filepath.Join(workDir, ".env")); err == nil {
-			viper.AddConfigPath(workDir)
-			break
-		}
 		if workDir == "/" {
 			break
 		}
+		viper.AddConfigPath(workDir)
 		workDir = filepath.Dir(workDir)
 	}
 
-	if err := viper.ReadInConfig(); err != nil && !errors.As(err, &viper.ConfigFileNotFoundError{}) {
-		panic(fmt.Errorf("failed to read config file: %w", err))
+	if err := viper.ReadInConfig(); err != nil {
+		if errors.As(err, &viper.ConfigFileNotFoundError{}) {
+			fmt.Printf(`{"level":"warn","msg":"config file not found", "path":"%s"}`+"\n", configPath)
+		} else {
+			panic(fmt.Errorf("failed to read config file: %w", err))
+		}
 	}
 }
